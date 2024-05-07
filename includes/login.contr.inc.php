6bbbbbfce86a7c363c8e997df8a0b2924fe54609
Zahar <zahratulnazirah@gmail.com>
2024-05-08 00:03:34 +0800

    "Add new helper functions to validate login credentials in login.contr.inc.php; update login.model.inc.php to return user data as associative array"
    The diff includes the following changes:
    
    * In login.

--- includes/login.contr.inc.php
+++ includes/login.contr.inc.php
@@ -1,1 +1,23 @@
 
+<?php
+
+declare(strict_type=1);
+function is_username_wrong(array|bool $result)
+{
+    if (!$result) {
+        return true;  //if the username doesn't exist,
+    } else {
+        return false;
+    }
+}
+
+
+
+function is_password_wrong(string $pwd, string $hashedPwd)
+{
+    if (!password_verify($pwd, $hashedPwd)) {
+        return true;  //if the username doesn't exist,
+    } else {
+        return false;
+    }
+}
--- includes/login.model.inc.php
+++ includes/login.model.inc.php
@@ -1,1 +1,14 @@
+<?php
 
+declare(strict_type=1);
+function get_user(object $pdo, string $username)
+{
+    $query = "SELECT * FROM tech_blog WHERE username = :username";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(":username", $username);
+    $stmt->execute();
+
+
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $result;
+}
