2dd4e95f003ea99c6a42c7e50e5479aa27977988
Zahar <87757637+zaharscript@users.noreply.github.com>
2024-05-07 12:48:40 +0800

    Update login.inc.php

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -4,7 +4,9 @@ if($_SERVER["REQUEST_METHOD"]==="POST"){
   $pwd = $_POST["password"];
 
   try{
-    
+    require_once 'dbh.inc.php';
+    require_once 'login_model.inc.php';
+    require_once 'login_view.inc.php';
   }catch(PDOexcetion $e){
     die("Query failed:".$e->getMessage());
   }
@@ -12,5 +14,29 @@ if($_SERVER["REQUEST_METHOD"]==="POST"){
   header("Location:../index.php");
   die();
 }
+
+//error handler
+        $errors = [];
+
+
+
+        if (is_input_empty($username, $pwd, $email)) {
+            $errors["empty_input"] = "Fill in all fields!";
+        }
+       
+        require_once 'config_session.inc.php'; //better to use this method then "session_start();"
+
+        if ($errors) {
+            $_SESSION["errors_signup"] = $errors;
+
+            $signupData = [
+                "username" => $username,
+                "email" => $email
+            ];
+            $_SESSION["signup_data"] = $signupData;
+
+            header("Location: ../index.php");
+            die();
+        }
 
 
